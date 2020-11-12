9ef874e18bb040967ce0384a77a5ce1be3e9dafa
Evgeny <chiburashka47@gmail.com>
2020-11-12 17:33:02 +0300

    init webpack

---
+++ dist/js/bundle.js
@@ -0,0 +1,14 @@
+/******/ (() => { // webpackBootstrap
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/*! unknown exports (runtime-defined) */
+/*! runtime requirements:  */
+let taskContainer = document.querySelectorAll(".list-group"),
+  task = document.querySelector(".list-group-item");
+
+taskContainer[1].append(task);
+
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
